6e9e0c76ba25945d3f928b40c1e0c4918a2dd23c
=Muhammad Ejaz Ul Haq <=ejazul.haq@strikeready.com>
2026-04-21 17:52:22 +0500

    docker file added

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print("hello world ")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "API is running"}
+
+@app.get("/test")
+def test():
+    return {"message": "test connection passed"}
+
+@app.get("/hello")
+def hello(name: str = "world"):
+    return {"message": f"hello {name}"}
